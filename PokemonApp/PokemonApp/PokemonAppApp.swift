de73b94f3dc70fc19045a3c33074d000089d36d0
Matheus Torres <30413727+matttheust@users.noreply.github.com>
2025-04-08 17:24:23 -0300

    first commit: add project assets

---
+++ PokemonApp/PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Matheus  Torres on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
